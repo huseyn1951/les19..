eb94501a4d395be25f27b706ff94ef4a0174c23e
huseyn1951 <sultanlkhuseyn@gmail.com>
2024-02-26 21:15:55 +0400

    tree

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            CalculateTreeLenght();
+
+        }
+        public static void CalculateTreeLenght()
+        {
+            int length = -1;
+            Console.WriteLine("input periods");
+            int periodinput= Convert.ToInt32(Console.ReadLine());
+            for (int period = 0; period <= periodinput; period++)
+            {
+                length += 1;
+                length *= 2; 
+            }
+
+            Console.WriteLine($"tree's length is {length}");
+        }
+    }
+}
---
+++ ConsoleApp28/Tree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Tree
+    {
+       public  int CalculateTreeLength(int periods)
+        {
+            int length = 1;
+            for (int period = 0; period <= periods; period++)
+            {
+                length *= 2;
+                length += period;
+            }
+            return length;
+
+            int treeLength = CalculateTreeLength(periods);
+            Console.WriteLine(treeLength);
+
+        }
+
+
+    }
+}
